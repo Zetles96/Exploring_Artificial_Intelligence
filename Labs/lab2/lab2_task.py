2202faa3bfc76bd0094ba724c22d9b0bcc9aac92
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2023-10-04 18:22:58 +0800

    added assignment one again after some fix stuff by tas

--- Labs/lab2/lab2_task.py
+++ Labs/lab2/lab2_task.py
@@ -110,31 +110,31 @@ class NaiveBayesClassifier:
         return test_predict
 
 
-if __name__ == '__main__':
-    train_dataset = np.load("train_dataset.npy")
-    test_dataset = np.load("test_dataset.npy")
-    train_labels = np.load("train_labels.npy")
-    test_labels = np.load("test_labels.npy")
-    nb_model = NaiveBayesClassifier()
-    nb_model.fit(train_dataset, train_labels)
-    print(f"After fitting the training data, the train size is\
-  {nb_model.train_size}, the number of features is {nb_model.num_features},\
-  the number of class labels is {nb_model.num_classes}.")  # should be 128, 6, 2
-    class_prior = nb_model.estimate_class_prior()
-    print(f"The class priors are {class_prior}.")  # should be [0.31538462 0.68461538]
-    likelihoods = nb_model.estimate_likelihoods()
-    print(
-        f"The likelihoods of the first feature (Age) are \n {likelihoods[0]}.")  # The rows are feature categories and the columns are label categories
-    # should be [[0.06666667 0.05376344]
-    #            [0.02222222 0.07526882]
-    #            [0.28888889 0.4516129 ]
-    #            [0.35555556 0.24731183]
-    #            [0.26666667 0.17204301]]
-    test_predict = nb_model.predict(test_dataset)
-    print(
-        f"The predictions for test data are:\n {test_predict}")  # should be [1 1 1 0 0 0 1 1 1 0 0 1 0 1 0 1 1 0 0 1 1 1 0 0 0 1 1 0 1 1 1 0 0 1 0 1 1
-    #            0 1 0 0 0 1 0 0 0 1 0 1 1 1 0 0 0 0]
-
-    accuracy_score = np.sum(test_predict == test_labels) / test_labels.shape[0]
-
-    print(accuracy_score)  # should be 0.67
+# if __name__ == '__main__':
+#     train_dataset = np.load("train_dataset.npy")
+#     test_dataset = np.load("test_dataset.npy")
+#     train_labels = np.load("train_labels.npy")
+#     test_labels = np.load("test_labels.npy")
+#     nb_model = NaiveBayesClassifier()
+#     nb_model.fit(train_dataset, train_labels)
+#     print(f"After fitting the training data, the train size is\
+#   {nb_model.train_size}, the number of features is {nb_model.num_features},\
+#   the number of class labels is {nb_model.num_classes}.")  # should be 128, 6, 2
+#     class_prior = nb_model.estimate_class_prior()
+#     print(f"The class priors are {class_prior}.")  # should be [0.31538462 0.68461538]
+#     likelihoods = nb_model.estimate_likelihoods()
+#     print(
+#         f"The likelihoods of the first feature (Age) are \n {likelihoods[0]}.")  # The rows are feature categories and the columns are label categories
+#     # should be [[0.06666667 0.05376344]
+#     #            [0.02222222 0.07526882]
+#     #            [0.28888889 0.4516129 ]
+#     #            [0.35555556 0.24731183]
+#     #            [0.26666667 0.17204301]]
+#     test_predict = nb_model.predict(test_dataset)
+#     print(
+#         f"The predictions for test data are:\n {test_predict}")  # should be [1 1 1 0 0 0 1 1 1 0 0 1 0 1 0 1 1 0 0 1 1 1 0 0 0 1 1 0 1 1 1 0 0 1 0 1 1
+#     #            0 1 0 0 0 1 0 0 0 1 0 1 1 1 0 0 0 0]
+#
+#     accuracy_score = np.sum(test_predict == test_labels) / test_labels.shape[0]
+#
+#     print(accuracy_score)  # should be 0.67
